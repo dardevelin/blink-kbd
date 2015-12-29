c93c4a4b41a811e2da2c8a975ae8db63b4f83e17
Darcy Brás da Silva <dardevelin@cidadecool.com>
2015-12-29 01:17:19 +0000

    blink-kbd.py is the plugin that handles light notifications for hexchat. lightcontroler.sh is set so that it controls the backlight of a keyboard more specifically it has been tested for asus n551j-cn220h.

---
+++ README.md
@@ -0,0 +1,22 @@
+#blink-kbd - hexchat plugin GPLv3
+---
+*blink-kbd* is a python plugin for *hexchat* which blinks the backlight
+of your keyboard or any other light controlled by lightcontroler.sh
+whenever you get highlighted/notification in hexchat.
+
+blink-kbd will leave the light on until hexchat regains
+focus/activity. Due to this, if the blinking has already started
+it will finished the cycle before consuming the notification.
+
+*lightcontroler.sh* by default works with keyboard backlight.
+I have only tested with *asus n551j-cn220h* laptop. However the plugin
+is made such that it invocates lightcontroler.sh with the commands
+'up' for *'lightON'* and 'down' for *'lightOFF'*. so as long you
+_change/replace lightcontroler.sh_ to suit your environment
+and maintain the behavior of *up* to represent _ON_ and *down* for _OFF_
+you shouldn't need to change anything else.
+
+
+##LICENSE - GPLv3 no later option. Ignore any references to later versions
+See LICENSE to know your rights or go to
+http://www.gnu.org/licenses/gpl-3.0.txt
---
+++ blink-kbd.py
@@ -0,0 +1,137 @@
+#!/bin/env python
+
+#### yes python is supreme and comes first
+# blink-kbd.py
+# This file is a plugin for the irc hexchat client that makes the keyboard
+# backlight blink once it has received a notification/highlight
+# Copyright (C) 2015  Darcy Bras da Silva
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/
+
+
+from __future__ import print_function
+
+import os
+from time import sleep
+import hexchat
+import thread
+
+__module_name__ = "kbd notify"
+__module_version__ = "1.1"
+__module_description__ = "Blinks the keyboard backlight on notifications"
+__module_author__ = "Darcy 'dardevelin' Bras da Silva"
+
+#None, True, False
+kbd_status = None
+kbd_light = False
+configdir = hexchat.get_info('configdir')
+thread_active = False
+
+def on():
+    global kbd_light
+    if False == kbd_light:
+        os.system('{}/addons/blink-kbd/lightcontroler.sh up'.format(configdir))
+        kbd_light = True
+    
+def off():
+    global kbd_light
+    if True == kbd_light:
+        os.system('{}/addons/blink-kbd/lightcontroler.sh down'.format(configdir))
+        kbd_light = False
+
+
+def blink(n, state=False):
+    global thread_active
+    global kbd_light
+    
+    idx = 0
+    while idx < n:
+        on()
+        sleep(1)
+        off()
+        sleep(1)
+        idx = idx + 1
+
+    if True == state:
+        on()
+
+    thread_active = False
+
+def dispatcher(word, word_eol, userdata):
+    global thread_active
+
+    # don't blink the keyboard if we are looking at the client
+    w_status = hexchat.get_info("win_status")
+    if 'active' == str(w_status).lower():
+        return
+    
+    if not thread_active:
+        thread_active = True
+        # we spawn a thread to not lock the client waiting
+        # the blinking effect to finish
+        thread.start_new_thread(blink, (3, True,))
+
+    return hexchat.EAT_NONE
+
+def consume_notification(i, ii):
+    global thread_active
+
+    # run endlessly until an opportunity is found to turn it off
+    while True:
+        if False == thread_active:
+            sleep(1)
+            off()
+            break
+        sleep(1)
+
+def notification_checked(word, word_eol, userdata):
+    global thread_active
+
+    # this tells that we have seen the notification and turns
+    # off the light, however  we need to wait until
+    # it finishes blinking, otherwise it will just be an off()
+    # out of 'the blink set'
+    if False == thread_active:
+        # already stopped, just turn it off
+        off()
+    else:
+        # don't lock the client for this. make a worker
+        thread.start_new_thread(consume_notification, (None,None))
+
+def unload_cb(userdata):
+    print("\0034 {} {} {}".format(__module_name__, __module_version__, "has been unloaded\003"))
+
+def toggle_kbd(word, word_eol, userdata):
+    global kbd_status
+    
+    if kbd_status == True:
+        kbd_status = False
+    else:
+        kbd_status = True
+
+    if kbd_status == True:
+        hexchat.prnt("kbd-notify is now at full speed")
+    else:
+        hexchat.prnt("kbd-notify is now suspended")
+
+def on_init_cb():
+    global status
+    kbd_status = True
+    hexchat.hook_print("Channel Msg Hilight", dispatcher, None, hexchat.PRI_LOWEST)
+    hexchat.hook_print("Focus Window", notification_checked, None, hexchat.PRI_LOWEST)
+    hexchat.hook_unload(unload_cb)
+    hexchat.hook_command("kbdlight", toggle_kbd)
+    print("\0034 {} {} {}".format(__module_name__, __module_version__,"has been loaded\003"))
+
+on_init_cb()
